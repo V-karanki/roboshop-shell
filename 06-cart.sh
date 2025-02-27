875f98cd9822d27f52ac301a7a97136cc40cfec8
V-karanki <venkateshbabu9.vv@gmail.com>
2025-02-26 18:21:42 -0600

    Roboshop-Code

--- 06-cart.sh
+++ 06-cart.sh
@@ -2,10 +2,11 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 
 dnf install nodejs -y
-cp cart.service /etc/systemd/system/cart.service
-
 
 useradd roboshop
+
+cp cart.service /etc/systemd/system/cart.service
+
 rm -rf /app
 mkdir /app
 
